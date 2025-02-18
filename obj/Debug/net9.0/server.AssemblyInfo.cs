edada7e3fb6c3474b87d09172f77462fbb0fb44f
nightfall-storm <bilalnnassernf@gmail.com>
2025-02-18 19:21:47 +0100

    initial server

--- obj/Debug/net9.0/server.AssemblyInfo.cs
+++ obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0849efe2839a446f8ddb41f1976b880fff1088b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b8481af7ec286f644d6ff5aac21c499b2b0d47")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
